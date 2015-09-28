81aa845941987031477bb6d90bc9ddf5353f2f0d
Mehmet Uyanik <Memuya@users.noreply.github.com>
2015-09-28 16:10:08 +1000

    Create example.php

---
+++ example.php
@@ -0,0 +1,54 @@
+
+<?php
+//require class
+require_once 'classes/Errors.php';
+
+//use namespace
+use Error\Errors;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error Handling</title>
+	<link rel="stylesheet" href="css/notice-boxes.css">
+</head>
+<body>
+	
+	<?php
+	/*****************
+	* FIRST SCENARIO *
+	******************/
+	$name = null;
+	$age = 0;
+
+	if(empty($name))
+		Errors::add("Name is empty");
+
+	if($age <= 0)
+		Errors::add("Age can not be 0");
+
+	//display the error messages or a success message
+	echo Errors::display("This message will show if no errors have been generated");
+
+	//clear the error messages so we can start fresh on the same page
+	Errors::clear();
+
+
+	/******************
+	* SECOND SCENARIO *
+	*******************/
+	$name = "Bob";
+	$age  = 50;
+
+	if(empty($name))
+		Errors::add("Name is empty");
+
+	if($age == 0)
+		Errors::add("Age can not be 0");
+
+	//Displays the success message because no errors have been added
+	echo Errors::display("This message will show if no errors have been generated");
+	?>
+</body>
+</html>
